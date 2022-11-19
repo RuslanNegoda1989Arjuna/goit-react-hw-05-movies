88cb8c84efa2106bd32d4ca62764fb426999c876
Ruslan Negoda <iarjuna108@gmail.com>
2022-11-19 16:13:56 +0200

    1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,26 @@
 import { HomeList } from 'components/HomeList';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import fetchTrending from 'tools/Api';
 
 export const Home = () => {
-  const trendFilms = fetchTrending().then(data => {
-    return data;
-  });
-  console.log('trendFilms', trendFilms);
+  const [trendFilms, setTrendFilms] = useState();
+
+  useEffect(() => {
+    try {
+      const films = fetchTrending();
+      // if (!films) {
+      //   return;
+      // }
+
+      films.then(data => {
+        return setTrendFilms(data);
+      });
+    } catch (error) {}
+  }, []);
+
+  console.log('state', trendFilms);
+
   return (
     <main>
       <HomeList trendFilms={trendFilms} />
--- src/components/HomeList.jsx
+++ src/components/HomeList.jsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
 
 export const HomeList = ({ trendFilms }) => {
-  console.log(trendFilms);
+  if (!trendFilms) {
+    return;
+  }
   return (
     <div>
       <ul>
-        {trendFilms.map(({ original_name }) => {
+        {trendFilms.map(({ original_name, original_title }) => {
+          console.log('original_name', original_name);
           return (
-            <li key={original_name}>
-              <Link to={`${original_name}`}>
-                <h3>{original_name}</h3>
+            <li key={original_name || original_title}>
+              <Link to={`${original_name || original_title}`}>
+                <h3>{original_name || original_title}</h3>
               </Link>
             </li>
           );
